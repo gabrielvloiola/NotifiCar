d5b934b3fb8bb03a546f2b9242791b43bf7b6d71
gabrielvloiola <gabrielvloiola@gmail.com>
2025-11-27 21:52:14 -0300

    adição area administrativa

--- app/src/main/java/com/example/notificar/MainActivity.kt
+++ app/src/main/java/com/example/notificar/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.example.notificar
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.notificar.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -26,7 +27,6 @@ class MainActivity : AppCompatActivity() {
             val email = binding.etEmail.text.toString()
             val password = binding.etPassword.text.toString()
 
-            // Validação e tentativa de login
             validator.performLogin(auth, email, password) { isSuccess ->
                 if (isSuccess) {
                     val intent = Intent(this, HomeActivity::class.java)
@@ -48,12 +48,17 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        // 🔹 Link para tela de recuperação de senha
+        // Link para recuperar senha
         binding.tvForgotPassword.setOnClickListener {
-            // A MainActivity apenas navega para a tela de recuperação.
             val intent = Intent(this, RecuperarSenhaActivity::class.java)
             startActivity(intent)
-            // A lógica de envio do e-mail está na RecuperarSenhaActivity.kt
+        }
+
+        // ✅ Link para acesso do admin (agora no lugar certo)
+        val tvAdminAccess = findViewById<TextView>(R.id.tvAdminAccess)
+        tvAdminAccess.setOnClickListener {
+            val intent = Intent(this, AdminLoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
